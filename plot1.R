27b4f1a748a5e0a4cd9bbb86c1d3444a3277530a
Ty Abonil <ty.abonil@gmail.com>
2014-05-09 18:34:45 -0400

    intial working code

---
+++ plot1.R
@@ -0,0 +1,11 @@
+plot1 <- function(){
+  data <- read.table("./rawdata/household_power_consumption.txt", sep=";", header=TRUE, colClasses=rep("character", 9))
+  data$dateTime <- paste(data$Date, data$Time)
+  data$dateTime <- as.POSIXct(data$dateTime, format="%d/%m/%Y %H:%M:%S")
+  data <- subset(data, dateTime >= as.POSIXct("01/02/2007 00:00:00", format="%d/%m/%Y %H:%M:%S") & dateTime <= as.POSIXct("02/02/2007 23:59:59", format="%d/%m/%Y %H:%M:%S"))
+  data[,3:9] <- sapply(data[, 3:9], as.numeric)
+  png("./figure/plot1.png", width=480, height=480)
+  hist(data$Global_active_power, col="red", xlab="Global Active Power (kilowatts)", main="Global Active Power")
+  dev.off()
+  data
+}
---
+++ plot3.R
@@ -0,0 +1,14 @@
+plot3 <- function(){
+  data <- read.table("./rawdata/household_power_consumption.txt", sep=";", header=TRUE, colClasses=rep("character", 9))
+  data$dateTime <- paste(data$Date, data$Time)
+  data$dateTime <- as.POSIXct(data$dateTime, format="%d/%m/%Y %H:%M:%S")
+  data <- subset(data, dateTime >= as.POSIXct("01/02/2007 00:00:00", format="%d/%m/%Y %H:%M:%S") & dateTime <= as.POSIXct("02/02/2007 23:59:59", format="%d/%m/%Y %H:%M:%S"))
+  data[,3:9] <- sapply(data[, 3:9], as.numeric)
+  png("./figure/plot3.png", width=480, height=480)
+  plot(data$dateTime, data$Sub_metering_1, type="l", ylab="Energy sub metering", xlab="")
+  lines(data$dateTime, data$Sub_metering_2, col="red")
+  lines(data$dateTime, data$Sub_metering_3, col="blue")
+  legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lty=1)
+  dev.off()
+  data
+}
